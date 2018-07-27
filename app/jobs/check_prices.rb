0fd90d652decb736604adcb0bb87f67297c1fe3a
Anca Sarbu <asarbu@everfi.com>
2018-07-27 16:46:43 +0300

    Updated database.yml file Transformed CheckPrices into rake task

--- app/jobs/check_prices.rb
+++
@@ -1,8 +0,0 @@
-class CheckPrices
-  def perform
-    Flight.find_each do |f|
-      p "Search flight fares for: #{f.departure_station} -> #{f.arrival_station}"
-      Flights::SearchFlightFareJob.new(f).perform
-    end
-  end
-end
